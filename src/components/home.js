3702710bacb129b4c8abe5bed3c20a20e7963c96
Abigail Swarth <abigailswarth@gmail.com>
2019-06-30 00:04:52 -0400

    date time

--- src/components/home.js
+++ src/components/home.js
@@ -68,7 +68,7 @@ export default class Home extends React.Component {
         city: arg.name,
       }
     })
-    console.log(this.state.weather)
+    console.log(arg)
   }
 
 
@@ -94,6 +94,26 @@ export default class Home extends React.Component {
     }
   }
 
+  getDateTime = () => {
+    var objToday = new Date(),
+      	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+      	dayOfWeek = weekday[objToday.getDay()],
+      	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+      	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+      	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+      	curMonth = months[objToday.getMonth()],
+      	curYear = objToday.getFullYear(),
+      	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+      	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+      	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+      	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+
+    var today = dayOfWeek + ", " + curMonth + " " + dayOfMonth + ", " + curYear;
+    var time = curHour + ":" + curMinute + curMeridiem;
+
+    return (<span className="day"><strong>{today}</strong><br/>{time}</span>);
+  }
+
 
   render() {
     return (
@@ -103,6 +123,10 @@ export default class Home extends React.Component {
                 handleChange={this.handleChange}
                 handleSubmit={this.handleSubmit}
         />
+
+      <div className="date">
+          {this.getDateTime()}
+        </div>
 
         <div>
           {!this.state.weather.temp ? <div className="enterCity">Please enter a city</div> : <Weather weather={this.state.weather} />}
